566531f83f1b4669f0d967c6e57712620d388ed9
Kim <kimly.jet@gmail.com>
2022-08-22 10:47:35 +0900

    Feat: Create and connect to database

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB Connection Successful!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.json());
 
