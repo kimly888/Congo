877804c0ad5ebeb0df3b725fa7089ca776c0f541
Kim <kimly.jet@gmail.com>
2022-08-22 15:59:34 +0900

    Feat: Add functionality for Admin to update product info

--- routes/product.js
+++ routes/product.js
@@ -14,37 +14,25 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-// // UPDATE USER INFO
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   // Handle password changes if any
-//   if (req.body.password) {
-//     // Reassign original password to updated and encrypted password
-//     req.body.password = CryptoJS.AES.encrypt(
-//       req.body.password,
-//       process.env.PASS_SECRET
-//     ).toString();
-//   }
+// UPDATE USER INFO
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    // Search database for product by id and update
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        // Set operator replaces the value of a field to the specified value
+        $set: req.body,
+      },
+      // New option returns document after update is applied
+      { new: true }
+    );
 
-//   try {
-//     // Search database for user by Id and update
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         // Set operator replaces the value of a field to the specified value
-//         $set: req.body,
-//       },
-//       // New option returns document after update is applied
-//       { new: true }
-//     );
-
-//     // Hide updated User password
-//     const { password, ...others } = updatedUser._doc;
-
-//     res.status(200).json(others);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // DELETE USER
 // router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
