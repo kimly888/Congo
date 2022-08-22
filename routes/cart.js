fb91c6d9897489fcc737901e10e326696b0f1558
Kim <kimly.jet@gmail.com>
2022-08-22 17:08:07 +0900

    Feat: Empty shopping cart

--- routes/cart.js
+++ routes/cart.js
@@ -34,15 +34,15 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-// // DELETE PRODUCT (ADMIN ONLY)
-// router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-//   try {
-//     await Product.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Product deleted!");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// DELETE CART
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id);
+    res.status(200).json("Cart emptied!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // GET ALL PRODUCTS
 // router.get("/", async (req, res) => {
