35625695cb6cf04af8f55dd2fd7fc3d6b4626cb9
Kim <kimly.jet@gmail.com>
2022-08-22 17:43:44 +0900

    Feat: Allow Admin to cancel an user order

--- routes/order.js
+++ routes/order.js
@@ -18,35 +18,35 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
-// // ADD TO CART
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     // Search database for product by id and update
-//     const cart = await Cart.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         // Set operator replaces the value of a field to the specified value
-//         $set: req.body,
-//       },
-//       // New option returns document after update is applied
-//       { new: true }
-//     );
+// UPDATE ORDER (ADMIN ONLY)
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    // Search database for product by id and update
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        // Set operator replaces the value of a field to the specified value
+        $set: req.body,
+      },
+      // New option returns document after update is applied
+      { new: true }
+    );
 
-//     res.status(200).json(cart);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(order);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-// // DELETE CART
-// router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
-//   try {
-//     await Cart.findByIdAndDelete(req.params.id);
-//     res.status(200).json("Cart emptied!");
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+// CANCEL ORDER (ADMIN ONLY)
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order cancelled!");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // // SHOW ALL USER CARTS (ADMIN ONLY)
 // router.get("/", verifyTokenAndAdmin, async (req, res) => {
