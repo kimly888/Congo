7d3b67ddeb2dcbb3b1a81f122b3156c76a484ea4
Kim <kimly.jet@gmail.com>
2022-08-22 16:52:07 +0900

    Feat: Add functionality to get by specified query

--- routes/product.js
+++ routes/product.js
@@ -47,11 +47,24 @@ router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 // GET ALL PRODUCTS
 router.get("/", async (req, res) => {
   const queryNew = req.query.new;
+  const queryCategory = req.query.category;
 
   try {
-    const products = queryNew
-      ? await Product.find().sort({ _id: -1 }).limit(5)
-      : await Product.find();
+    let products;
+
+    if (queryNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (queryCategory) {
+      // In operator selects products where the category of a field equals any product that has that category in the specified categories array
+      products = await Product.find({
+        categories: {
+          $in: [queryCategory],
+        },
+      });
+    } else {
+      // Show all products if no query is given
+      products = await Product.find();
+    }
 
     res.status(200).json(products);
   } catch (err) {
