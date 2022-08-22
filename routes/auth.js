f6429c54072ea37a05bf00e4fe8d009951b004c8
Kim <kimly.jet@gmail.com>
2022-08-22 11:03:14 +0900

    Feat: Create request to register new users

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,29 @@
-// Log in process should have it's own router for authentication
 const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  // Get new user info
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    // Use crypto-js to hash user passwords (cipher AES)
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString(),
+  });
+
+  try {
+    // Save new user using save method
+    const savedUser = await newUser.save();
+
+    // Send saved user to client side
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
