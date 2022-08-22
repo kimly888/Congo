ef600400560176b2fd51c8220a8ba8d29d633ca9
Kim <kimly.jet@gmail.com>
2022-08-22 12:24:36 +0900

    Feat: Add function to verify user tokens

---
+++ routes/verifyToken.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        // If token is not valid or has expired
+        res.status(403).json("Token is not valid!");
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).json("You are not authenticated!");
+  }
+};
