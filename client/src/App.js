28327700423b86c5f3235fefe79d94c371fd2e39
Kim <kimly.jet@gmail.com>
2022-08-23 11:54:57 +0900

    Feat: Set hero template

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Header/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="home">
       <Navbar />
+      <Hero />
     </div>
   );
 }
