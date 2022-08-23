f172d269a6e4ddbcac185474740a7126f2330f11
Kim <kimly.jet@gmail.com>
2022-08-23 13:25:21 +0900

    Feat: Add Sign-up component template

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Header/Navbar";
 import Hero from "./components/Hero";
 import Login from "./components/Login";
+import Signup from "./components/Sign-up";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       {/* <Login /> */}
+      {/* <Signup/> */}
     </div>
   );
 }
